f657041f6cc7c7e5b712fdcc279c24d39390c101
Jay <jordanchu95@gmail.com>
2020-02-10 15:06:16 -0500

    fixed testing issues for user

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,9 +2,9 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
+    sequence(:name) { |n| "Rob#{n}"}
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
   end
-
 end
