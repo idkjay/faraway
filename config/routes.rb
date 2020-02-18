425736b00a4a34efd4188f398d0a8e72debc8507
Jay <jordanchu95@gmail.com>
2020-02-17 19:00:28 -0500

    kiwi api

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :planners, only: [:index, :create, :destroy, :update]
-      post '/', to: 'images#search_destination'
+      # post '/', to: 'images#search_destination'
+      resources :flights, only: [:index]
     end
   end
 end
