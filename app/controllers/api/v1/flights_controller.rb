58e4e092b031734fdce8ed6faa430dd49a29390c
Jay <jordanchu95@gmail.com>
2020-02-17 19:29:52 -0500

    api v1 call for kiwi

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -3,24 +3,16 @@ class Api::V1::FlightsController < ApplicationController
 
   def index
 
-    # placeOriginSearch
-    # placeDestinationSearch
-    # startDate.strftime(%d/%m/%Y)
-    # endDate
-    # dateFrom = date_from.gsub("-","/")
-    # dateTo = date_to.gsub("-","/")
-
-
     dateFrom = Date.parse(params[:date_from]).strftime("%d/%m/%Y")
     dateTo = Date.parse(params[:date_to]).strftime("%d/%m/%Y")
     flyFrom = params[:flyFrom]
     to = params[:to]
-    binding.pry
+
 
     url = "https://api.skypicker.com/flights?flyFrom=#{flyFrom}&to=#{to}&date_from=#{dateFrom}&date_to=#{dateTo}&sort=price&partner=picky&limit=3"
     response = HTTParty.get(url)
     parsed = JSON.parse(response.body)
-    binding.pry
+
     render json: parsed
   end
 end
