2975de19176edaad96b0c9977e503f8dd1b705c1
Jay <jordanchu95@gmail.com>
2020-02-19 15:22:41 -0500

    removed some issues from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
-  get '/', to: 'static_pages#index'
   get '/planners', to: 'static_pages#index'
   get '/flights_search', to: 'static_pages#index'
 
@@ -16,7 +15,6 @@ Rails.application.routes.draw do
       resources :planners, only: [:index, :create, :destroy, :update]
       post '/flights_search', to: 'flights#search'
       resources :flights, only: [:index]
-      resources :images, only: [:index]
     end
   end
 end
